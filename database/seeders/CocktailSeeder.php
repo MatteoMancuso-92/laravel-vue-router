c70704ecdbeaff47e2be9c6f598e6218b2446a08
gianlucaligori <gianluca.ligori.95@gmail.com>
2023-07-20 15:25:57 +0200

    commit 1 gian

--- database/seeders/CocktailSeeder.php
+++ database/seeders/CocktailSeeder.php
@@ -4,6 +4,7 @@ namespace Database\Seeders;
 
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
+use App\Models\Cocktail;
 
 class CocktailSeeder extends Seeder
 {
@@ -14,6 +15,17 @@ class CocktailSeeder extends Seeder
      */
     public function run()
     {
-        //
+        foreach (config('cocktails') as $cocktail) {
+
+            Cocktail::create([
+                'title' => $cocktail['title'],
+                'description' => $cocktail['description'],
+                'thumb' => $cocktail['thumb'],
+                'price' => $cocktail['price'],
+                'series' => $cocktail['series'],
+                'sale_date' => $cocktail['sale_date'],
+                'type' => $cocktail['type'],
+            ]);
+        }
     }
 }
